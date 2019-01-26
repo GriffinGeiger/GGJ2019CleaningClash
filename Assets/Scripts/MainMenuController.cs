a4e9aed8953852234f600a046268690cb80a56b0
will-payne <bigwill2015@gmail.com>
2019-01-26 00:15:07 -0500

    Implemented Main Menu UI
    Start button currently does nothing - need to decide whether to go to game  scene or extra bedroom setup scene. Instructions/controls and credits implemented

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    public GameObject instructionsPanel;
+    public GameObject creditsPanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        instructionsPanel.SetActive(false);
+        creditsPanel.SetActive(false);
+    }
+
+    public void DisplayInstructions()
+    {
+        instructionsPanel.SetActive(true);
+    }
+
+    public void DisplayCredits()
+    {
+        creditsPanel.SetActive(true);
+    }
+
+    public void ReturnToMenu()
+    {
+        instructionsPanel.SetActive(false);
+        creditsPanel.SetActive(false);
+    }
+
+    public void StartGame()
+    {
+        //SceneManager.LoadScene("BedroomSetupScene");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
