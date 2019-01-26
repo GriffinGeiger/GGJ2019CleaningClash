e30c37f5da5f01d000ea5e8d0183b55780ed0158
Lauren Hoadley <lehoadle@ncsu.edu>
2019-01-26 16:44:39 -0500

    Done with Sun

--- Assets/Scripts/SunMovement.cs
+++ Assets/Scripts/SunMovement.cs
@@ -4,11 +4,18 @@ using UnityEngine;
 
 public class SunMovement : MonoBehaviour
 {
-    float DegreePerSec = 90.0f / 180.0f;
+    static float TimeInSec = 180f;
+    float DegreePerSec = 90.0f / TimeInSec;
 
 
     // Update is called once per frame
-    void Update() =>
-        // Spin the object around the world origin at 20 degrees/second.
-        transform.RotateAround(Vector3.zero, Vector3.back, DegreePerSec * Time.deltaTime);
+    void Update()
+    {
+        // Timer for match.
+        TimeInSec -= Time.deltaTime;
+        if (TimeInSec > 0) { 
+            // Spin the object around the world origin at 20 degrees/second.
+            transform.RotateAround(Vector3.zero, Vector3.back, DegreePerSec * Time.deltaTime);
+        }
+    }
 }
