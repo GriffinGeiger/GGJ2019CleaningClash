3ef649edd4bc5bc80ba06d7da298be54ee4766b4
Lauren Hoadley <lehoadle@ncsu.edu>
2019-01-26 17:58:31 -0500

    Angle Adjustments

--- Assets/Scripts/SunMovement.cs
+++ Assets/Scripts/SunMovement.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class SunMovement : MonoBehaviour
 {
     static float TimeInSec = 180f;
-    float DegreePerSec = 90.0f / TimeInSec;
+    float DegreePerSec = 1000f / TimeInSec;
 
 
     // Update is called once per frame
@@ -15,7 +15,7 @@ public class SunMovement : MonoBehaviour
         TimeInSec -= Time.deltaTime;
         if (TimeInSec > 0) { 
             // Spin the object around the world origin at 20 degrees/second.
-            transform.RotateAround(Vector3.zero, Vector3.back, DegreePerSec * Time.deltaTime);
+            transform.RotateAround(Vector3.up, Vector3.back, DegreePerSec * Time.deltaTime);
         }
     }
 }
