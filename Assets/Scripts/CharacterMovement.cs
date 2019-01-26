721762e10a141396d1af273588c341f169b46476
GriffinGeiger <griffin2420@yahoo.com>
2019-01-26 00:59:56 -0500

    Bindings, Input scripts, Movement scripts

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class CharacterMovement : MonoBehaviour
 {
     public Rigidbody2D m_rigidbody;
+    private float m_stunTime = 0;
+    private bool m_stunned = false;
+    //public Item m_heldItem;
 
     // Start is called before the first frame update
     void Start()
@@ -18,8 +21,52 @@ public class CharacterMovement : MonoBehaviour
         
     }
 
-    public void Move(double xVelocity, double yVelocity)
+    public void Interact()
     {
+        //check different things that can be interacted with and interact accordingly
+
+        //if hand is empty, fill hand 
+        if(/*m_heldItem == null*/ false)
+        {
+
+        }
+        else //holding item
+        {
+            if(/*In range of item*/false)
+            {
+                //call pick up on item
+            }
+
+        }
+
+        //in range of object and hands are empty?
+        //pick up object
         
+    }
+
+
+    public void Move(Vector2 velocity) //should be called in FixedUpdate
+    {
+        //check status effects to see if possible to move
+        if(m_stunned)
+        {
+            velocity = Vector2.zero;
+        }
+        //if it is possible to move then move according to the input
+        m_rigidbody.velocity = velocity;
+    }
+
+    public void Stun(float stunTime) //other things call this to stun the character
+    {
+        m_stunTime = stunTime;
+        m_stunned = true;
+        StartCoroutine("StunCoroutine");
+    }
+
+    private IEnumerator StunCoroutine()
+    {
+        yield return new WaitForSeconds(m_stunTime);
+        m_stunTime = 0;
+        m_stunned = false;
     }
 }
---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerInput : MonoBehaviour
+{
+    public enum playerTag { Player1 = 1 , Player2 =2 , Player3 = 3 , Player4 = 4 };
+    public playerTag m_playerTag; 
+    public CharacterMovement m_controller;
+    
+
+    private Vector2 m_joystickMovement= new Vector2();
+    private bool m_interactionButton;
+    // Update is called once per frame
+    void Update()
+    {
+        m_joystickMovement.x = Input.GetAxisRaw("Player" + m_playerTag + "_Joystick_x");
+        m_joystickMovement.y = Input.GetAxisRaw("Player" + m_playerTag + "_Joystick_y");
+        m_interactionButton  = Input.GetButtonDown("Player" + m_playerTag + "_interaction"); 
+    }
+
+    private void FixedUpdate()
+    {
+        //might need to check state of game here
+        if(m_controller != null)
+            m_controller.Move(m_joystickMovement); //move character
+
+        if (m_interactionButton)
+        {
+            m_controller.Interact();
+            m_interactionButton = false;
+        }
+    }
+}
