b4401a863bce9b613bcd7f77c7fd77692fbcc818
Lauren Hoadley <lehoadle@ncsu.edu>
2019-01-26 16:00:02 -0500

    rotating sun now works

--- Assets/Scripts/SunMovement.cs
+++ Assets/Scripts/SunMovement.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class SunMovement : MonoBehaviour
 {
+    float DegreePerSec = 90.0f / 180.0f;
+
 
     // Update is called once per frame
     void Update() =>
         // Spin the object around the world origin at 20 degrees/second.
-        transform.RotateAround(Vector3.zero, Vector3.back, 20 * Time.deltaTime);
+        transform.RotateAround(Vector3.zero, Vector3.back, DegreePerSec * Time.deltaTime);
 }
