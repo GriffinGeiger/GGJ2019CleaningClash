8de74998f2ba9777fc67b8ae30b39bc298fdf4fd
GriffinGeiger <griffin2420@yahoo.com>
2019-01-25 22:50:57 -0500

    Saved sample scene

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public Rigidbody2D m_rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Move(double xVelocity, double yVelocity)
+    {
+        
+    }
+}
