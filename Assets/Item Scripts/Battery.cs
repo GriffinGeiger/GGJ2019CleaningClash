d464dadccda5ffc68b9f16ec7fb86a922d3a8557
Tony Mosolf <t.mosolf@comcast.net>
2019-01-26 01:32:39 -0500

    Made basic scripts for various item types

---
+++ Assets/Item Scripts/Battery.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Battery :Item
+{
+    //Placing the battery into the fan
+    void Place()
+    {
+
+    }
+}
---
+++ Assets/Item Scripts/Throwable.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Throwable : Item
+{
+    //Indicator if object is moving through the air
+    protected bool InAir;
+
+    //Indicator if object has been thrown
+    protected bool Thrown;
+    //Zero movement vector for comparison
+    Vector3 stop = new Vector3(0, 0, 0);
+
+    //Shoots the item in the given angle with specified strength
+    void Throw (float strength, Vector2 angle)
+    {
+        GetComponent<Rigidbody>().isKinematic = true;
+        GetComponent<Rigidbody>().AddForce(angle * strength);
+        Thrown = true;
+    }
+
+    void Update()
+    {
+        if (GetComponent<Rigidbody>().velocity == stop)
+        {
+            InAir = false;
+        }
+        else
+        {
+            InAir = true;
+        }
+    }
+}
