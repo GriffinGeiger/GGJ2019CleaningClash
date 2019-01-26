b90c5f64e9cc1c059dee24c01c2e054ebb3b31a1
Lauren Hoadley <lehoadle@ncsu.edu>
2019-01-26 14:52:23 -0500

    Started on Sun Mechanics

---
+++ Assets/Scripts/SunMovement.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SunMovement : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Spin the object around the world origin at 20 degrees/second.
+        transform.RotateAround(Vector1.zero, Vector1.up, 20 * Time.deltaTime);
+    }
+}
