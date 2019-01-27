5213ae403c323f48ccf29f4900be6f2be691711a
GriffinGeiger <griffin2420@yahoo.com>
2019-01-27 01:33:01 -0500

    Going to sleep

--- Assets/Scripts/SunMovement.cs
+++ Assets/Scripts/SunMovement.cs
@@ -8,6 +8,7 @@ public class SunMovement : MonoBehaviour
     const float totalDegrees = 120f;
     static float TimeInSec = 180f;
     float DegreePerSec = totalDegrees/ TimeInSec;
+    public bool sunMoving = false;
 
     public void StartSunTimer()
     {
@@ -17,10 +18,13 @@ public class SunMovement : MonoBehaviour
     void Update()
     {
         // Timer for match.
-        TimeInSec -= Time.deltaTime;
-        if (TimeInSec > 0) { 
-
-            transform.RotateAround(Vector3.up, Vector3.back, DegreePerSec * Time.deltaTime);
+        if (sunMoving)
+        {
+            TimeInSec -= Time.deltaTime;
+            if (TimeInSec > 0)
+            {
+                transform.RotateAround(Vector3.up, Vector3.back, DegreePerSec * Time.deltaTime); //rotate sun at degreepersec rate each second
+            }
         }
     }
 }
