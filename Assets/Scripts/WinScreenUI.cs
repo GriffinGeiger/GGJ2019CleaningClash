6c5d70e6caed7978605b6e1f4c0b71ad0b2109e6
will-payne <bigwill2015@gmail.com>
2019-01-27 15:36:50 -0500

    WinnerUI implemented

---
+++ Assets/Scripts/WinScreenUI.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class WinScreenUI : MonoBehaviour
+{
+    public GameObject winnerPanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        winnerPanel.SetActive(false);
+    }
+
+    public void DisplayWinner(string winner)
+    {
+        winnerPanel.SetActive(true);
+        GameObject winnerText = GameObject.FindGameObjectWithTag("WinnerText");
+        Text text = winnerText.GetComponent<Text>();
+        text.text = winner + "Wins!";
+    }
+
+    public void Rematch()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void Menu()
+    {
+        SceneManager.LoadScene("MainMenuScene");
+    }
+}
