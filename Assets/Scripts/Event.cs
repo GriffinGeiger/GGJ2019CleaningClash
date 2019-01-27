3eb61405b12bd39b49121686435a8f1aefd854c1
Tony Mosolf <t.mosolf@comcast.net>
2019-01-27 10:59:10 -0500

    Refactoring event generation and system

--- Assets/Scripts/Event.cs
+++ Assets/Scripts/Event.cs
@@ -1,188 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Event : MonoBehaviour
 {
-
-    Random rand;
-    public Text timer = null;
-
     public enum Events { Lava, Puppy, Darkness };
 
-    private Events current;
+    public Events current;
 
-    //An algorithm for using RNG to select an option from a weighted number set
-    float Choose (float[] probs)
+    public Event(Events type)
     {
-        float total = 0;
-        foreach (float elem in probs)
-        {
-            total += elem;
-        }
-
-        float randomPoint = Random.value * total;
-
-        for (int i=0; i < probs.Length; i++)
-        {
-            if (randomPoint < probs[i])
-            {
-                return i;
-            }
-            else
-            {
-                randomPoint -= probs[i];
-            }
-        }
-        return probs.Length - 1;
+        current = type;
     }
 
-    //Method for choosing a random event to be created
-    Events chooseEvent()
+    public void execute()
     {
-        float[] probs = { 50f, 30f, 10f };
-        float choice = Choose(probs);
-        switch ((int)choice)
+        switch (current)
         {
-            case 0:
-                return Events.Lava;
-            case 1:
-                return Events.Puppy;
-            case 2:
-                return Events.Darkness;
+            case Events.Lava:
+                FloorIsLava();
+                break;
+            case Events.Puppy:
+                WhoLetTheDogOut();
+                break;
+            case Events.Darkness:
+                LightsOut();
+                break;
             default:
-                return Events.Darkness;
+                FloorIsLava();
+                break;
         }
     }
-
-    //Method for choosing a random item to be created
-    Item chooseItem()
-    {
-        //              common, lego, socks
-        float[] probs = { 80f, 10f, 10f };
-        float choice = Choose(probs);
-        switch ((int)choice)
-        {
-            case 0:
-                return new Item();
-            case 1:
-                return new Legos();
-            case 2:
-                return new Socks();
-            default:
-                return new Item();
-        }
-
-
-    }
-
-    //Method for choosing a random powerup to be created
-    Powerups.PowerUp choosePowerUp()
-    {
-        float[] probs = { 10f, 10f, 10f, 10f, 10f, 1f };
-        float choice = Choose(probs);
-        switch ((int)choice)
-        {
-            case 0:
-                return Powerups.PowerUp.CellPhone;
-            case 1:
-                return Powerups.PowerUp.GasMask;
-            case 2:
-                return Powerups.PowerUp.HulkHands;
-            case 3:
-                return Powerups.PowerUp.InfinityGauntlet;
-            case 4:
-                return Powerups.PowerUp.SugarRush;
-            case 5:
-                return Powerups.PowerUp.SpawnBattery;
-            default:
-                return Powerups.PowerUp.SpawnBattery; ;
-        }
-    }
-
 
     void FloorIsLava()
     {
-        current = Events.Lava;
-        StartCoroutine("EventStart");
-        timer = null;
+        LavaController con = new LavaController();
     }
 
     void WhoLetTheDogOut()
     {
-        current = Events.Puppy;
-        StartCoroutine("EventStart");
-        timer = null;
         Dog puppy = new Dog();
     }
 
     void LightsOut()
     {
-        current = Events.Darkness;
-        StartCoroutine("EventStart");
-        timer = null;
         //Remove all light except for from window
-    }
-
-    IEnumerator EventStart()
-    {
-        string eventName = "";
-        switch (current)
-        {
-            case Events.Lava:
-                eventName = "Floor is lava";
-                break;
-            case Events.Puppy:
-                eventName = "Who let the dogs out";
-                break;
-            case Events.Darkness:
-                eventName = "Lights out";
-                break;
-        }
-        string time = " is starting in 5 seconds";
-        timer.text = eventName + time;
-        yield return new WaitForSeconds(5.0f);
-    }
-
-    //Randomly spawns items on each team's side
-    void SpawnItems(int amount)
-    {
-        for (int i = 0; i < amount; i++)
-        {
-            for (int j = 0; j < 2; j++)
-            {
-                int min = j == 0 ? 0 : Screen.width / 2;
-                int max = j == 0 ? Screen.width / 2 : Screen.width;
-               
-                //              common, lego, socks
-                float[] probs = { 80f, 10f, 10f };
-                
-                int num = (int)Choose(probs);
-                while (true)
-                {
-                    Vector2 loc = new Vector2(Random.Range(min, max), Random.Range(0, Screen.height));
-                    
-                    if (Physics2D.OverlapCircle(loc, .5f) == null){
-                        if (num == 0)
-                        {
-                            InstantPrefabs.SpawnRandomCommonThrowable(loc);
-                        }
-                        /**
-                         * else if (num == 1) {
-                         *     InstantPrefabs.SpawnLegos(loc);
-                         * }
-                         * 
-                         * else{
-                         *      InstantPrefabs.SpawnSocks(loc);
-                         * }
-                         * 
-                         **/
-                        break;
-                    }
-                }
-
-            }
-        }
     }
 }
---
+++ Assets/Scripts/EventManager.cs
@@ -0,0 +1,226 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EventManager : MonoBehaviour
+{
+
+    Random rand;
+    public Text timer = null;
+
+    //                    lava,  dog, lights
+    float[] EventProbs = { 50f,  30f,  10f };
+
+    //                  common, legos, socks
+    float[] ItemProbs = { 80f,   10f,   10f };
+
+    //                      sugar, gauntlet, hands, battery, mask, phone
+    float[] PowerupProbs = { 10f,     10f,    10f,    2f,     10f,   1f };
+
+    
+
+    //An algorithm for using RNG to select an option from a weighted number set
+    int Choose (float[] probs)
+    {
+        float total = 0;
+        foreach (float elem in probs)
+        {
+            total += elem;
+        }
+
+        float randomPoint = Random.value * total;
+
+        for (int i=0; i < probs.Length; i++)
+        {
+            if (randomPoint < probs[i])
+            {
+                return i;
+            }
+            else
+            {
+                randomPoint -= probs[i];
+            }
+        }
+        return probs.Length - 1;
+    }
+
+    //Method for choosing a random event to be created
+    Event.Events chooseEvent()
+    {
+        int choice = Choose(EventProbs);
+        switch (choice)
+        {
+            case 0:
+                return Event.Events.Lava;
+            case 1:
+                return Event.Events.Puppy;
+            case 2:
+                return Event.Events.Darkness;
+            default:
+                return Event.Events.Darkness;
+        }
+    }
+
+    //Method for choosing a random item to be created
+    Item chooseItem()
+    {
+        int choice = Choose(ItemProbs);
+        switch (choice)
+        {
+            case 0:
+                return new Item();
+            case 1:
+                return new Legos();
+            case 2:
+                return new Socks();
+            default:
+                return new Item();
+        }
+
+
+    }
+
+    //Method for choosing a random powerup to be created
+    Powerups.PowerUp choosePowerUp()
+    {
+        int choice = Choose(PowerupProbs);
+        switch (choice)
+        {
+            case 0:
+                return Powerups.PowerUp.SugarRush;
+            case 1:
+                return Powerups.PowerUp.InfinityGauntlet;
+            case 2:
+                return Powerups.PowerUp.HulkHands;
+            case 3:
+                return Powerups.PowerUp.SpawnBattery;
+            case 4:
+                return Powerups.PowerUp.GasMask;
+            case 5:
+                return Powerups.PowerUp.CellPhone;
+            default:
+                return Powerups.PowerUp.SpawnBattery;
+        }
+    }
+
+    IEnumerator EventStart(Event e)
+    {
+        string eventName = "";
+        Event.Events current = e.current;
+        switch (current)
+        {
+            case Event.Events.Lava:
+                eventName = "Floor is lava";
+                break;
+            case Event.Events.Puppy:
+                eventName = "Who let the dogs out";
+                break;
+            case Event.Events.Darkness:
+                eventName = "Lights out";
+                break;
+        }
+        string time = " is starting in 5 seconds";
+        timer.text = eventName + time;
+        yield return new WaitForSeconds(5.0f);
+    }
+
+    //Randomly spawns items on each team's side
+    void SpawnItems(int amount)
+    {
+        for (int i = 0; i < amount; i++)
+        {
+            for (int j = 0; j < 2; j++)
+            {
+                int min = j == 0 ? 0 : Screen.width / 2;
+                int max = j == 0 ? Screen.width / 2 : Screen.width;
+               
+                int num = Choose(ItemProbs);
+                while (true)
+                {
+                    Vector2 loc = new Vector2(Random.Range(min, max), Random.Range(0, Screen.height));
+                    
+                    if (Physics2D.OverlapCircle(loc, .5f) == null){
+                        if (num == 0)
+                        {
+                            InstantPrefabs.SpawnRandomCommonThrowable(loc);
+                        }
+                        /**
+                         * else if (num == 1) {
+                         *     InstantPrefabs.SpawnLegos(loc);
+                         * }
+                         * 
+                         * else{
+                         *      InstantPrefabs.SpawnSocks(loc);
+                         * }
+                         * 
+                         **/
+                        break;
+                    }
+                }
+
+            }
+        }
+    }
+
+    //Randomly spawns a powerup on each team's side
+    void SpawnPowerup()
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            int min = j == 0 ? 0 : Screen.width / 2;
+            int max = j == 0 ? Screen.width / 2 : Screen.width;
+
+            int num = Choose(PowerupProbs);
+            while (true)
+            {
+                Vector2 loc = new Vector2(Random.Range(min, max), Random.Range(0, Screen.height));
+
+                if (Physics2D.OverlapCircle(loc, .5f) == null)
+                {
+
+                    /**
+                    * 
+                    * 
+                    if (num == 0){
+                        InstantPrefabs.SpawnSugarRush(loc);
+                        break;
+                    }
+                    else if (num == 1){
+                        InstantPrefabs.InfinityGauntlet(loc);
+                        break;
+                    }
+                    else  if (num == 2){
+                        InstantPrefabs.SpawnHulkHands(loc);
+                        break;
+                    }
+                    else if (num == 3){
+                        InstantPrefabs.SpawnBattery(loc);
+                        break;
+                    }
+                    else if (num == 0){
+                        InstantPrefabs.SpawnGasMask(loc);
+                        break;
+                    }
+                    else {
+                        InstantPrefabs.SpawnCellPhone(loc);
+                        break;
+                    }
+                    **/
+
+                }
+            }
+
+        }
+        
+    }
+
+    //Randomly spawns a powerup on each team's side
+    void ChooseAndStartEvent()
+    {
+        Event e = new Event(chooseEvent());
+        StartCoroutine("EventStart", e);
+        
+
+    }
+}
