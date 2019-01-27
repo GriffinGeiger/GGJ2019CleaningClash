8caa5a66d22b2b3738652e39942463202436ee05
will-payne <bigwill2015@gmail.com>
2019-01-27 10:59:00 -0500

    Sunlight functionality implemented

--- Assets/Scripts/SunMovement.cs
+++ Assets/Scripts/SunMovement.cs
@@ -9,6 +9,10 @@ public class SunMovement : MonoBehaviour
     static float TimeInSec = 180f;
     float DegreePerSec = totalDegrees/ TimeInSec;
     public bool sunMoving = false;
+    public GameObject light;
+    public Sprite morning;
+    public Sprite afternoon;
+    public Sprite night;
 
     public void StartSunTimer()
     {
@@ -25,6 +29,18 @@ public class SunMovement : MonoBehaviour
             {
                 transform.RotateAround(Vector3.up, Vector3.back, DegreePerSec * Time.deltaTime); //rotate sun at degreepersec rate each second
             }
+        }
+        if(TimeInSec >= 60f)
+        {
+            light.gameObject.GetComponent<SpriteRenderer>().sprite = morning;
+        }
+        else if(TimeInSec >= 10f)
+        {
+            light.gameObject.GetComponent<SpriteRenderer>().sprite = afternoon;
+        }
+        else
+        {
+            light.gameObject.GetComponent<SpriteRenderer>().sprite = night;
         }
     }
 }
