e01e9bb1abcd0b519d0b6da72c7c509f7f9ef0b6
Tony Mosolf <t.mosolf@comcast.net>
2019-01-26 20:33:12 -0500

    Created Events

---
+++ Assets/Scripts/Event.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    Random rand;
+    public Text timer = null;
+
+    public enum Events { Lava, Puppy, Darkness };
+
+    private Events current;
+
+
+    void FloorIsLava()
+    {
+        current = Events.Lava;
+        StartCoroutine("EventStart");
+        timer = null;
+    }
+
+    void WhoLetTheDogOut()
+    {
+        current = Events.Puppy;
+        StartCoroutine("EventStart");
+        timer = null;
+        Dog puppy = new Dog();
+    }
+
+    void LightsOut()
+    {
+        current = Events.Darkness;
+        StartCoroutine("EventStart");
+        timer = null;
+        //Remove all light except for from window
+    }
+
+    IEnumerator EventStart()
+    {
+        string eventName = "";
+        switch (current)
+        {
+            case Events.Lava:
+                eventName = "Floor is lava";
+                break;
+            case Events.Puppy:
+                eventName = "Who let the dogs out";
+                break;
+            case Events.Darkness:
+                eventName = "Lights out";
+                break;
+        }
+        string time = " is starting in 5 seconds";
+        timer.text = eventName + time;
+        yield return new WaitForSeconds(5.0f);
+    }
+
+    void SpawnItems()
+    {
+        
+        //for each side of the room
+            //Randomly pick a spot to place the item
+            //If it would overlap with something, pick a different one
+    }
+}
